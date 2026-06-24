64b557de6a353303352fe97563e7593e4f0707fb
likitha <nlikitha022@gmail.com>
2026-06-24 14:44:02 +0530

    initial maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello likitha!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello likitha" );
+    }
+}
